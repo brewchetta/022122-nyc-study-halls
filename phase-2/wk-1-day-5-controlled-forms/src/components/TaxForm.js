4206bc810158d32abbdf5500135f907921e183ba
brewchetta <chett.tiller@gmail.com>
2022-03-18 12:04:28 -0400

    woo taxes!

--- phase-2/wk-1-day-5-controlled-forms/src/components/TaxForm.js
+++ phase-2/wk-1-day-5-controlled-forms/src/components/TaxForm.js
@@ -2,11 +2,72 @@ import { useState } from 'react'
 
 function TaxForm(props) {
 
+  const [income, setIncome] = useState(0)
+  const [taxBracket, setTaxBracket] = useState("low")
+  const [comments, setComments] = useState("THE IRS IS GREAT")
+
+  // const [formState, setFormState] = useState({
+  //   income: 0,
+  //   "tax-bracket": 'low',
+  //   comments: 'I REALLY REALLY REALLY **** the IRS'
+  // })
+
+  // const {income, taxBracket, comments} = formState
+
   function handleSubmit(e) {
     e.preventDefault()
-    console.log('Form is being submitted!')
+    switch(taxBracket) {
+      case "low": {
+        alert(`You owe $${income * 0.5} this year! Thank you for paying your taxes!`)
+        break;
+      }
+      case "middle": {
+        alert(`You owe $${income * 0.2} this year! Thank you for paying your taxes!`)
+        break;
+      }
+      case "high": {
+        alert(`You owe $${income * 0.01} this year! Thank you for paying your taxes!`)
+        break;
+      }
+      default: 
+        alert('Something went wrong')
+    }
+
+    handleReset()
   }
 
+  function handleIncomeChange(e) {
+    if (e.target.value >= 0) {
+      // setFormState({...formState, income: e.target.value})
+      setIncome(e.target.value)
+    } else {
+      alert('You cant have negative income!!!')
+    }
+  }
+
+  function handleReset() {
+    setComments('THE IRS IS GREAT!')
+    setIncome(0)
+    setTaxBracket('low')
+    // setFormState({
+    //   income: 0,
+    //   taxBracket: 'low',
+    //   comment: 'I REALLY REALLY REALLY **** the IRS'
+    // })
+  }
+
+  // function handleChangeBracket(e) {
+  //   setFormState({...formState, taxBracket: e.target.value})
+  // }
+
+  // function handleChangeComment(e) {
+  //   setFormState({...formState, comment: e.target.value})
+  // }
+ 
+  // function handleChange(e) {
+  //   setFormState({...formState, [e.target.name]: e.target.value})
+  // }
+ 
   return (
 
     <>
@@ -14,26 +75,26 @@ function TaxForm(props) {
     <form onSubmit={handleSubmit}>
 
       <label name="income">How much did you make last year?</label>
-      <input type="number" name="income" />
+      <input type="number" name="income" onChange={handleIncomeChange} value={income} />
 
       <p>Based on your income, we'd suggest a tax bracket of <span>Service Drone</span></p>
 
       <label htmlFor="tax-bracket">What is your tax bracket?</label>
 
-      <select name="tax-bracket">
+      <select name="tax-bracket" onChange={e => setTaxBracket(e.target.value)} value={taxBracket}>
         <option value="low">Service Drone</option>
         <option value="middle">Middle Class American</option>
         <option value="high">Been to Space</option>
       </select>
 
       <label htmlFor="comments">We value your feedback! Write a comment: </label>
-      <textarea name="comments"/>
+      <textarea name="comments" onChange={e => setComments(e.target.value)} value={comments} />
 
       <input type="submit" value="Submit Taxes" />
 
     </form>
 
-    <button type="button">Reset Form</button>
+    <button type="button" onClick={handleReset}>Reset Form</button>
 
     </>
 
