6c13dd27049247309d47e2dd16f59c136670c971
brewchetta <chett.tiller@gmail.com>
2022-03-21 13:05:38 -0400

    added react-router lecture

---
+++ phase-2/wk-2-day-5-react-router/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './style/index.css';
+import App from './components/App';
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
