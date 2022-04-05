b78f4eee22454fc13e6b288c7da98ff527be3900
brewchetta <chett.tiller@gmail.com>
2022-04-05 13:54:15 -0400

    additional notes for lecture 2

--- phase-3/02-ruby-classes.rb
+++ phase-3/02-ruby-classes.rb
@@ -6,76 +6,6 @@ require 'pry'
 
 # Why use Object Oriented Programming?
 
-class VideoGame
-
-  # attr_writer :name, :playtime
-  attr_reader :playtime
-  attr_accessor :name, :genre
-
-  def initialize(name, playtime)
-    @name = name
-    @playtime = playtime
-    @@all_videogames << self
-  end
-
-  # class method
-
-  @@all_videogames = []
-
-  def self.all_videogames
-    @@all_videogames
-  end
-
-  def self.what_am_i?
-    self.class
-  end
-
-  # instance method
-
-  def boast
-    puts "OMG #{@name} IS SOOOO COOOL! I'VE PLAYED FOR #{@playtime} HOURS!"
-  end
-
-
-  # instance variables
-
-  # SETTER
-  # def name=(name)
-  #   @name = name
-  # end
-  #
-  def playtime=(playtime)
-    if playtime < 10000
-      @playtime = playtime
-    else
-      puts "That's too much time get a life"
-    end
-  end
-
-  # GETTER
-  # def name
-  #   @name
-  # end
-
-  # def playtime
-  #   @playtime
-  # end
-
-end
-
-super_mario = VideoGame.new('SUPER MARIO BROS.', 1000)
-sonic = VideoGame.new('SONIC', 900)
-
-binding.pry
-
-
-
-
-
-
-
-
-
 # What is the difference between an instance and a class?
 
 # How do we create an instance?
@@ -93,3 +23,64 @@ binding.pry
 # What is self? How can we leverage self in OOP?
 
 # What is implicit self?
+
+class VideoGame
+
+  attr_reader :playtime
+  attr_accessor :name, :genre
+
+  # GETTER / READER
+  # def name
+  #   @name
+  # end
+
+  # def playtime
+  #   @playtime
+  # end
+
+  # SETTER / WRITER
+  # def name=(name)
+  #   @name = name
+  # end
+
+  # example of a custom setter
+  def playtime=(playtime)
+    if playtime < 10000
+      @playtime = playtime
+    else
+      puts "That's too much time get a life"
+    end
+  end
+
+  # INITIALIZE - fires when someone uses VideoGame.new(name, playtime)
+
+  def initialize(name, playtime)
+    @name = name
+    @playtime = playtime
+    @@all_videogames << self
+  end
+
+  # EXAMPLE INSTANCE METHOD
+
+  def boast
+    puts "OMG #{@name} IS SOOOO COOOL! I'VE PLAYED FOR #{@playtime} HOURS!"
+  end
+
+  # CLASS METHODS / VARIABLES
+
+  @@all_videogames = []
+
+  def self.all_videogames
+    @@all_videogames
+  end
+
+  def self.what_am_i?
+    self.class
+  end
+
+end
+
+super_mario = VideoGame.new('SUPER MARIO BROS.', 1000)
+sonic = VideoGame.new('SONIC', 900)
+
+binding.pry
