c87b8779e7185f53b3fbc95db0679e771725eebb
brewchetta <chett.tiller@gmail.com>
2022-04-05 09:49:10 -0400

    notes for study hall

---
+++ phase-3/02-ruby-classes.rb
@@ -0,0 +1,15 @@
+# 02 Ruby Classes and OOP
+
+# What is Object Oriented Programming?
+
+# Why use Object Oriented Programming?
+
+# What is the difference between an instance and a class?
+
+# How do we create an instance?
+
+# How do we create methods for our instance?
+
+# How can we give our instance properties? How can we access those properties? How can we make them changeable?
+
+# What is a macro?
