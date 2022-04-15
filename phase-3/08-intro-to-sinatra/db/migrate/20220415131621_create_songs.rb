49d39c50312b705f3dcf214cab21aaf096099241
brewchetta <chett.tiller@gmail.com>
2022-04-15 10:11:55 -0400

    seed data

--- phase-3/08-intro-to-sinatra/db/migrate/20220415131621_create_songs.rb
+++ phase-3/08-intro-to-sinatra/db/migrate/20220415131621_create_songs.rb
@@ -3,6 +3,7 @@ class CreateSongs < ActiveRecord::Migration[6.1]
     create_table :songs do |t|
       t.string :title
       t.integer :playtime_in_seconds
+      t.integer :album_id
 
       t.timestamps
     end
--- phase-3/08-intro-to-sinatra/db/seeds.rb
+++ phase-3/08-intro-to-sinatra/db/seeds.rb
@@ -7,42 +7,42 @@ a2 = Album.create!(title: "Ring-a-Ding Ding!", year: 1960)
 puts ".\n"
 
 print "Creating songs."
-a1.songs.create(title: "Come Fly With Me", playtime_in_seconds: 199)
+a1.songs.create!(title: "Come Fly With Me", playtime_in_seconds: 199)
 print "."
-a1.songs.create(title: "Around The World", playtime_in_seconds: 201)
+a1.songs.create!(title: "Around The World", playtime_in_seconds: 201)
 print "."
-a1.songs.create(title: "Isle Of Capri", playtime_in_seconds: 149)
+a1.songs.create!(title: "Isle Of Capri", playtime_in_seconds: 149)
 print "."
-a1.songs.create(title: "Moonlight In Vermont", playtime_in_seconds: 212)
+a1.songs.create!(title: "Moonlight In Vermont", playtime_in_seconds: 212)
 print "."
-a1.songs.create(title: "Autumn In New York", playtime_in_seconds: 276)
+a1.songs.create!(title: "Autumn In New York", playtime_in_seconds: 276)
 print "."
-a1.songs.create(title: "On The Road To Mandalay", playtime_in_seconds: 210)
+a1.songs.create!(title: "On The Road To Mandalay", playtime_in_seconds: 210)
 print "."
-a1.songs.create(title: "Let's Get Away From It All", playtime_in_seconds: 131)
+a1.songs.create!(title: "Let's Get Away From It All", playtime_in_seconds: 131)
 print "."
-a1.songs.create(title: "April In Paris", playtime_in_seconds: 171)
+a1.songs.create!(title: "April In Paris", playtime_in_seconds: 171)
 print "."
-a1.songs.create(title: "London By Night", playtime_in_seconds: 211)
+a1.songs.create!(title: "London By Night", playtime_in_seconds: 211)
 print "."
 
-a2.songs.create(title: "Ring-A-Ding Ding", playtime_in_seconds: 166)
+a2.songs.create!(title: "Ring-A-Ding Ding", playtime_in_seconds: 166)
 print "."
-a2.songs.create(title: "Let's Fall in Love", playtime_in_seconds: 133)
+a2.songs.create!(title: "Let's Fall in Love", playtime_in_seconds: 133)
 print "."
-a2.songs.create(title: "Be Careful, It's My Heart", playtime_in_seconds: 126)
+a2.songs.create!(title: "Be Careful, It's My Heart", playtime_in_seconds: 126)
 print "."
-a2.songs.create(title: "A Foggy Day", playtime_in_seconds: 138)
+a2.songs.create!(title: "A Foggy Day", playtime_in_seconds: 138)
 print "."
-a2.songs.create(title: "A Fine Romance", playtime_in_seconds: 133)
+a2.songs.create!(title: "A Fine Romance", playtime_in_seconds: 133)
 print "."
-a2.songs.create(title: "In the Still of the Night", playtime_in_seconds: 206)
+a2.songs.create!(title: "In the Still of the Night", playtime_in_seconds: 206)
 print "."
-a2.songs.create(title: "The Coffee Song", playtime_in_seconds: 173)
+a2.songs.create!(title: "The Coffee Song", playtime_in_seconds: 173)
 print "."
-a2.songs.create(title: "When I Take My Sugar To Tea", playtime_in_seconds: 126)
+a2.songs.create!(title: "When I Take My Sugar To Tea", playtime_in_seconds: 126)
 print "."
-a2.songs.create(title: "Let's Face The Music And Dance", playtime_in_seconds: 179)
+a2.songs.create!(title: "Let's Face The Music And Dance", playtime_in_seconds: 179)
 puts ".\n"
 
 puts "🎶 Seeding complete! 🎶"
