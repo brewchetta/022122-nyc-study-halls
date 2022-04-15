1a5691f7e9caecc4c525d5b2736d8fcdba426f94
brewchetta <chett.tiller@gmail.com>
2022-04-15 12:06:16 -0400

    frank sinatra

--- phase-3/08-intro-to-sinatra/app/controllers/application_controller.rb
+++ phase-3/08-intro-to-sinatra/app/controllers/application_controller.rb
@@ -1,7 +1,27 @@
 class ApplicationController < Sinatra::Base
 
   get '/' do
-    "Hello world"
+    "Hello Frank"
+  end
+
+  get '/hamburgers' do
+    "<h1>We love hamburgers</h1>"
+  end
+
+  # INDEX
+  get '/albums' do
+    Album.all.to_json
+  end
+
+  # INDEX
+  get '/songs' do
+    Song.all.to_json
+  end
+
+  # SHOW
+  get '/songs/:id' do
+    song = Song.find(params[:id])
+    song.to_json(only: :title)
   end
 
 end
